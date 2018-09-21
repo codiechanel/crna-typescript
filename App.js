5f120528542f9df04188c67bd190750bc5062bee
codiechanel <codiechanel@gmail.com>
2018-09-21 20:03:13 +0800

    cool

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
-import * as React from "react";
-import { StatusBar } from "react-native";
-import { Container, Content, Button, List, ListItem, Text } from "native-base";
-import Expo from "expo";
+import * as React from "react"
+import { StatusBar } from "react-native"
+import { Container, Content, Button, List, ListItem, Text } from "native-base"
+import Expo from "expo"
 // import Home from "./src/Home";
-import style from "./src/styles";
-import Home from "./src/Home";
+import style from "./src/styles"
+import Home from "./src/Home"
 
 export default class App extends React.Component {
   // state: { loading: boolean; };
   constructor(props) {
-    super(props);
-    this.state = { loading: true };
+    super(props)
+    this.state = { loading: true }
   }
 
   async componentWillMount() {
@@ -18,14 +18,14 @@ export default class App extends React.Component {
       Roboto: require("native-base/Fonts/Roboto.ttf"),
       Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
       Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf")
-    });
-    this.setState({ loading: false });
+    })
+    this.setState({ loading: false })
   }
 
   render() {
     if (this.state.loading) {
-      return <Expo.AppLoading />;
+      return <Expo.AppLoading />
     }
-    return <Home />;
+    return <Home />
   }
 }
